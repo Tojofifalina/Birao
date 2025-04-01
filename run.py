0549e91d665b3f1a7a5667a7154a39b23dc08f3d
Tojofifalina <156069207+Tojofifalina@users.noreply.github.com>
2025-04-01 15:22:09 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
+from my_project import create_app  # Assurez-vous que ce module existe
 import os
-from django.core.management import execute_from_command_line
+
+app = create_app()
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Birao.settings")  # Remplacez `myproject` par le nom de votre projet
-    execute_from_command_line(["manage.py", "runserver", "192.168.43.1:8000"])
-#pyinstaller --onefile --add-data "myproject/settings.py;myproject" run.py
-#pyinstaller --onefile --hidden-import="django" run.py
+    port = int(os.environ.get("PORT", 8000))  # Render attribue un port dynamique
+    app.run(host="0.0.0.0", port=port, debug=False)
