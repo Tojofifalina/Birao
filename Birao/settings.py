ef62a5bd1379e66d60b392fed55feea065cab92c
Tojofifalina <156069207+Tojofifalina@users.noreply.github.com>
2025-04-02 21:32:19 +0300

    Update settings.py

--- Birao/settings.py
+++ Birao/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os.path
 from pathlib import Path
 import django_heroku
-import dj_databade_url
+#import dj_databade_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -158,5 +158,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals())
+#DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#django_heroku.settings(locals())
